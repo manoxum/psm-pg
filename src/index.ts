9acb93f306c73acb39df94d913345c3068ba54c2
zootakuxy <danielcarvalho555@gmail.com>
2025-11-06 12:30:26 +0000

    @psm

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import {PSMDriver, ModelOptions, PSMGenerator, PSMMigrationResult, PSMExecute} from "@prisma-psm/core";
+import {PSMDriver, ModelOptions, PSMGenerator, PSMMigrationResult, PSMExecute, CustomScript} from "@prisma-psm/core";
 import {parser} from "./parser/parser";
 import {migrate,migrated} from "./migration";
 import {sql} from "./parser/sql";
 import {dump} from "./dump";
+import fs from "fs";
 
 
 
@@ -10,18 +11,50 @@ function prepare( model:ModelOptions ){
     if( !model.schema ) model.schema = "public";
 }
 
+function executeRaw( scripts: CustomScript[] ): string{
+    const sql:string[] = [];
+    if( !!scripts && !Array.isArray( scripts ) ) throw new Error(`dsds ds dsdsdsds ${typeof scripts}`);
+    if( !scripts || !Array.isArray(scripts) || !scripts.length ) return null;
+    scripts.forEach(value => {
+        const filename:string = value.filename;
+        sql.push(`-- =============================== ${value.filename } ========================================` );
+        sql.push(`do $$ begin  raise notice '%', format( 'Iniciando a execução do script ${filename}...'); end; $$;`);
+        sql.push( value.raw.endsWith(";")? value.raw: `${value.raw};`)
+        sql.push(`do $$ begin  raise notice '%', format( 'Iniciando a execução do script ${filename}... [OK]'); end; $$;`);
+        sql.push("" );
+        sql.push("" );
+        sql.push("" );
+    });
+    return sql.join("\n");
+}
 
 const driver :PSMDriver = {
     migrated:( opts )=>{
         return migrated( opts );
     },
-    migrator: opts => ({
-        migrate:() => migrate({ sql: opts.migrate, url: opts.url, label: "NEXT" }),
+    migrator: opts =>({
+        migrate:( custom ) => {
+            const sql = [];
+            sql.push( opts.migrate );
+            [ "functions", "triggers", "views" ].forEach( group => {
+                const raw = executeRaw( custom?.resources?.[group])
+                if( !!raw ) sql.push( raw );
+            });
+            const n = sql.join(";\n");
+
+            fs.writeFileSync( "dsdsds.sql", n )
+            return migrate({ sql: n, url: opts.url, label: "NEXT" });
+        },
         test:() => migrate({ sql: opts.check, url: opts.url, label: "TEST" }),
         core:() => migrate({ sql: opts.core, url: opts.url, label: "CORE" }),
         dump:() => dump( opts ),
-        async execute(str: string): Promise<PSMExecute> {
-            const execute = await migrate({ sql: str, url: opts.url, label: "EXECUTE" });
+        executeRaw,
+        async execute(str: CustomScript[]): Promise<PSMExecute> {
+            const execute = await migrate({
+                sql: executeRaw(str),
+                url: opts.url,
+                label: "EXECUTE"
+            });
             return {
                 error: execute.error,
                 messages: execute.messages,
@@ -32,7 +65,6 @@ const driver :PSMDriver = {
 
     generator:(opts) => {
         let response = parser(opts);
-
         const generator: PSMGenerator = {
             migrate: () => sql( { mode: "migrate" }, response),
             check: () => sql( { mode: "check" }, response),
